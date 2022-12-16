feee01732fb9a2d73eb099349e58a1e786015d48
HanGyulKang <gang6607@gmail.com>
2022-12-16 20:12:24 +0900

    querydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -18,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -117,6 +119,37 @@ class MemberRepositoryTest {
         assertThat(result1.getContent())
                 .extracting("username")
                 .containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+
+        // 아주 편리한 기능이지만 Join관계가 있는 환경에서는 사용하기가 매우 까다로워서 실제 운영 환경에서는 사용을 권하지 않는다.
+        // Join이 불가능 함.
+        // 복잡한 실무 환경에서는 사용 한계가 명확하다.
+        Iterable<Member> members = memberRepository
+                .findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for(Member findMember : members) {
+            System.out.println("member1 = " + findMember.toString());
+        }
     }
 
     @Test
